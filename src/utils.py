c83483722ced8fe09d19f303df0c42838955e207
Max Taylor-Davies <maxtaylordavies@gmail.com>
2023-02-18 12:10:19 +0000

    first step: try and run custom DT data collator

---
+++ src/utils.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import os
+
+import torch
+
+
+def log(msg, outPath=None):
+    """Prints a message to the console and optionally writes it to a file.
+
+    Args:
+        msg (str): The message to print.
+        outPath (str) (optional): The path to the file to write the message to.
+    """
+    msg = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {msg}"
+    print(msg)
+    if outPath is not None:
+        with open(outPath, "a") as f:
+            f.write(msg + "\n")
+
+
+def setupGPU(seed=None):
+    useCuda = torch.cuda.is_available()
+
+    device = torch.device("cuda" if useCuda else "cpu")
+    log(f"Using device: {torch.cuda.get_device_name()}")
+
+    if not seed:
+        seed = torch.randint(0, 2**32, (1,)).item()
+        log(f"You did not set seed, so {seed} was chosen")
+
+    torch.manual_seed(seed)
+    if useCuda:
+        device_str = (
+            f"{device.type}:{device.index}" if device.index else f"{device.type}"
+        )
+        os.environ["CUDA_VISIBLE_DEVICES"] = device_str
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        # This does make things slower :(
+        torch.backends.cudnn.benchmark = False
