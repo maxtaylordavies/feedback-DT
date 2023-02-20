0bc5325a50e54cf7eff606d29d756cdbb6ed8940
Sabrina <sabrina.mccallum@outlook.com>
2023-02-20 20:09:06 +0000

    add files for creating d4rl-style datasets for BabyAI envs with Minari

---
+++ src/argparsing.py
@@ -0,0 +1,15 @@
+import argparse
+
+def get_dataset_args():
+    parser = argparse.ArgumentParser(description='Get admin token, mission and variant')
+    
+    parser.add_argument('--env_name', type=str,
+                        help='the name of the environment; must be registered with gymnasium')
+    parser.add_argument('--num_episodes', type=int, nargs=1, default=10,
+                        help='the number of episodes to collect for the environment')
+    parser.add_argument('--include_timeout', type=bool, nargs=1, default=True,
+                        help='whether to include episodes terminated by timeout / truncated')
+    
+    args = parser.parse_args()
+
+    return vars(args)
