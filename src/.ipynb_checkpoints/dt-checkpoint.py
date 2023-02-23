6e4f4423d42a754930ee2a82c9f87a6910466677
Sabrina McCallum <sabrina.mccallum@outlook.com>
2023-02-23 18:25:59 +0000

    add file for quickly inspecting attributes of babyai MinariDatasets

---
+++ src/.ipynb_checkpoints/dt-checkpoint.py
@@ -0,0 +1,73 @@
+import torch
+from transformers import (
+    DecisionTransformerModel,
+)
+
+from get_datasets import load_dataset
+
+class TrainableDT(DecisionTransformerModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def forward(self, **kwargs):
+        output = super().forward(**kwargs)
+        # add the DT loss
+        action_preds = output[1]
+        action_targets = kwargs["actions"]
+        attention_mask = kwargs["attention_mask"]
+        act_dim = action_preds.shape[2]
+        action_preds = action_preds.reshape(-1, act_dim)[attention_mask.reshape(-1) > 0]
+        action_targets = action_targets.reshape(-1, act_dim)[
+            attention_mask.reshape(-1) > 0
+        ]
+
+        loss = torch.mean((action_preds - action_targets) ** 2)
+
+        return {"loss": loss}
+
+    def original_forward(self, **kwargs):
+        return super().forward(**kwargs)
+
+    # function that gets an action from the model using autoregressive prediction with a window of the previous 20 timesteps.
+    def get_action(self, states, actions, rewards, returns_to_go, timesteps):
+        # This implementation does not condition on past rewards
+
+        states = states.reshape(1, -1, self.config.state_dim)
+        actions = actions.reshape(1, -1, self.config.act_dim)
+        returns_to_go = returns_to_go.reshape(1, -1, 1)
+        timesteps = timesteps.reshape(1, -1)
+
+        states = states[:, -self.config.max_length :]
+        actions = actions[:, -self.config.max_length :]
+        returns_to_go = returns_to_go[:, -self.config.max_length :]
+        timesteps = timesteps[:, -self.config.max_length :]
+
+        # pad all tokens to sequence length
+        padding = self.config.max_length - states.shape[1]
+        attention_mask = torch.cat([torch.zeros(padding), torch.ones(states.shape[1])])
+        attention_mask = attention_mask.to(dtype=torch.long).reshape(1, -1)
+
+        states = torch.cat(
+            [torch.zeros((1, padding, self.config.state_dim)), states], dim=1
+        ).float()
+        actions = torch.cat(
+            [torch.zeros((1, padding, self.config.act_dim)), actions], dim=1
+        ).float()
+        returns_to_go = torch.cat(
+            [torch.zeros((1, padding, 1)), returns_to_go], dim=1
+        ).float()
+        timesteps = torch.cat(
+            [torch.zeros((1, padding), dtype=torch.long), timesteps], dim=1
+        )
+
+        state_preds, action_preds, return_preds = self.original_forward(
+            states=states,
+            actions=actions,
+            rewards=rewards,
+            returns_to_go=returns_to_go,
+            timesteps=timesteps,
+            attention_mask=attention_mask,
+            return_dict=False,
+        )
+
+        return action_preds[0, -1]
---
+++ src/.ipynb_checkpoints/train-checkpoint.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+import os
+
+from get_datasets import load_dataset
+from transformers import (
+    DecisionTransformerConfig,
+    Trainer,
+    TrainingArguments,
+)
+import wandb
+
+from src.argparsing import get_training_args
+from src.data import DecisionTransformerGymDataCollator
+from src.dt import TrainableDT
+from src.utils import log, setup_devices, is_network_connection
+from src.visualiser import visualise_trained_model
+
+
+def load_data_and_create_model():
+    # load the dataset
+    dataset = load_dataset(
+        "edbeeching/decision_transformer_gym_replay", "halfcheetah-expert-v2"
+    )
+    collator = DecisionTransformerGymDataCollator(dataset["train"])
+
+    # create the model
+    config = DecisionTransformerConfig(
+        state_dim=collator.state_dim, act_dim=collator.act_dim
+    )
+    model = TrainableDT(config)
+
+    return dataset, collator, model
+
+
+def train_model(args, dataset, collator, model):
+    # initialise the training args
+    training_args = TrainingArguments(
+        run_name=args["run_name"],
+        output_dir=args["output"],
+        report_to=None if args["disable_wandb"] else "wandb",
+        logging_steps=args["log_interval"],
+        remove_unused_columns=False,
+        num_train_epochs=args["epochs"],
+        per_device_train_batch_size=args["batch_size"],
+        learning_rate=args["lr"],
+        weight_decay=1e-4,
+        warmup_ratio=0.1,
+        optim="adamw_torch",
+        max_grad_norm=0.25,
+    )
+
+    # initialise the trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset["train"],
+        data_collator=collator,
+    )
+
+    # train the model
+    log("Starting training...")
+    trainer.train()
+    log("Training complete :)")
+
+    return model
+
+
+def main(args):
+    # do some setup
+    if not args["run_name"]:
+        args["run_name"] = f"dt-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+        log(f"run_name not specified, using {args['run_name']}")
+    
+    if args["wandb_mode"] is not "disabled":
+        os.environ["WANDB_PROJECT"] = "feedback-DT"
+        os.environ["WANDB_LOG_MODEL"] = "true"
+        os.environ["WANDB_WATCH"] = "false"
+
+        if args["wandb_mode"] is "offline" or not is_network_connection():
+            os.environ["WANDB_MODE"] = "dryrun"
+
+    setup_devices(not args["no_gpu"], args["seed"])
+
+    # load the data and create the model
+    dataset, collator, model = load_data_and_create_model()
+
+    # train the model
+    model = train_model(args, dataset, collator, model)
+
+    # visualise the trained model
+    visualise_trained_model(args, collator, model, "HalfCheetah-v4")
+
+    # finish the wandb run
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    args = get_training_args()
+    log(f"parsed args: {args}")
+    main(args)
