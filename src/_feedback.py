b872f6757e8ff9e84840f52ba5508a13b3589070
Sabrina <sabrina.mccallum@outlook.com>
2023-03-10 12:22:39 +0000

    Refactor feedback code so that feedback is generated in super class

--- src/_feedback.py
+++ src/_feedback.py
@@ -10,21 +10,21 @@ from scipy.spatial.distance import cdist
 from argparsing import get_args
 from _datasets import get_dataset, name_dataset
 
-OBJECT_TO_IDX = {
-    "unseen": 0,
-    "empty": 1,
-    "wall": 2,
-    "floor": 3,
-    "door": 4,
-    "key": 5,
-    "ball": 6,
-    "box": 7,
-    "goal": 8,  # goal world object does not exist in BabyAI environments
-    "lava": 9,
-    "agent": 10,  # agent does not seem to be included as an object in BabyAI environment grids
+OBJECT_TO_STR = {
+    0: "unseen",
+    1: "empty",
+    2: "wall",
+    3: "floor",
+    4: "door",
+    5: "key",
+    6: "ball",
+    7: "box",
+    8: "goal",  # goal world object does not exist in BabyAI environments
+    9: "lava",
+    10: "agent",  # agent does not seem to be included as an object in BabyAI environment grids
 }
 
-COLOR_TO_IDX = {"red": 0, "green": 1, "blue": 2, "purple": 3, "yellow": 4, "grey": 5}
+COLOR_TO_STR = {0: "red", 1: "green", 2: "blue", 3: "purple", 4: "yellow", 5: "grey"}
 
 ACTION_TO_STR = {
     0: "left",
@@ -51,7 +51,7 @@ class Feedback(ABC):
         self.dataset_name = name_dataset(args)
         self.dataset = dataset
 
-        self.goal_color, self.goal_object = self._get_goal_metadata()
+        self.goal_color, self.goal_type = self._get_goal_metadata()
 
         self.episode_data = self._get_episode_data()
 
@@ -94,9 +94,42 @@ class Feedback(ABC):
     def _get_polarity(self):
         raise NotImplementedError
 
-    @abstractmethod
-    def generate_feedback(self):
-        raise NotImplementedError
+    def generate_feedback(self, attribute):
+        with open(
+            f"{os.path.abspath('')}/feedback_variants.json", encoding="utf-8"
+        ) as json_file:
+            feedback_variants = json.load(json_file)[self.feedback_type]
+
+        self.feedback_data[self.feedback_type][self.feedback_mode] = {
+            self.feedback_freq: []
+        }
+        for e, episode in enumerate(self.episode_data[attribute]):
+            episode_feedback = []
+            if self.feedback_freq_type.lower() == "poisson":
+                feedback_freq = 0
+                while feedback_freq == 0:
+                    feedback_freq = np.random.poisson(self.feedback_freq_steps)
+            else:
+                feedback_freq = self.feedback_freq_steps
+            for i, attribute_value in enumerate(episode):
+                if i == 0 and self.feedback_type == "distance":
+                    self._save_previous_agent_position(attribute_value)
+                    episode_feedback.append("")
+                    continue
+                if (i + 1) % feedback_freq == 0:
+                    polarity = self._get_polarity(attribute_value, e, i)
+                    feedback = feedback_variants[polarity][self.feedback_mode]
+                    if not isinstance(feedback, str):
+                        random_id = self.rng.integers(len(feedback))
+                        random_feedback = feedback[random_id]
+                        episode_feedback.append(random_feedback)
+                    else:
+                        episode_feedback.append(feedback)
+                else:
+                    episode_feedback.append("")
+            self.feedback_data[self.feedback_type][self.feedback_mode][
+                self.feedback_freq
+            ].append(episode_feedback)
 
     def save_feedback(self):
         feedback_path = f"{os.path.abspath('')}/feedback_data/{self.dataset_name}.json"
@@ -170,52 +203,26 @@ class DirectionFeedback(Feedback):
         else:
             return False
 
-    def _get_polarity(self, direction_observation, relative_goal_position):
+    def _get_polarity(self, direction_observation, current_episode, current_step):
+        relative_goal_position = self._get_relative_goal_position(
+            current_episode, current_step
+        )
         if self._facing_goal(direction_observation, relative_goal_position):
             return "positive"
         else:
             return "negative"
 
     def generate_feedback(self):
-        with open(
-            f"{os.path.abspath('')}/feedback_variants.json", encoding="utf-8"
-        ) as json_file:
-            feedback_variants = json.load(json_file)[self.feedback_type]
-
-        self.feedback_data[self.feedback_type][self.feedback_mode] = {
-            f"{self.feedback_freq_type}_{self.feedback_freq_steps}": []
-        }
-        for e, episode in enumerate(self.episode_data["direction_observations"]):
-            episode_feedback = []
-            if self.feedback_freq_type.lower() == "poisson":
-                feedback_freq = 0
-                while feedback_freq == 0:
-                    feedback_freq = np.random.poisson(self.feedback_freq_steps)
-            else:
-                feedback_freq = self.feedback_freq_steps
-            for i, direction_observation in enumerate(episode):
-                if i % feedback_freq == 0:
-                    relative_goal_position = self._get_relative_goal_position(e, i)
-                    polarity = self._get_polarity(
-                        direction_observation, relative_goal_position
-                    )
-                    feedback = feedback_variants[polarity][self.feedback_mode]
-                    if not isinstance(feedback, str):
-                        random_id = self.rng.integers(len(feedback))
-                        random_feedback = feedback[random_id]
-                        episode_feedback.append(random_feedback)
-                    else:
-                        episode_feedback.append(feedback)
-                else:
-                    episode_feedback.append("")
-            self.feedback_data[self.feedback_type][self.feedback_mode][
-                self.feedback_freq
-            ].append(episode_feedback)
+        return super().generate_feedback("direction_observations")
 
 
 class DistanceFeedback(Feedback):
     def __init__(self, *args):
         super().__init__(*args)
+        self.previous_agent_position = None
+
+    def _save_previous_agent_position(self, agent_position):
+        self.previous_agent_position = agent_position
 
     def _get_distance(self, goal_position, agent_position):
         return cdist(
@@ -224,49 +231,21 @@ class DistanceFeedback(Feedback):
             metric="cityblock",
         )[0][0]
 
-    def _get_polarity(self, d_current, d_previous):
+    def _get_polarity(self, agent_position, current_episode, current_step):
+        goal_position = self.episode_data["goal_positions"][current_episode][
+            current_step
+        ]
+        current_agent_position = agent_position
+        d_current = self._get_distance(goal_position, current_agent_position)
+        d_previous = self._get_distance(goal_position, self.previous_agent_position)
+        self.previous_agent_position = current_agent_position
         if d_current < d_previous:
             return "positive"
         else:
             return "negative"
 
     def generate_feedback(self):
-        with open(
-            f"{os.path.abspath('')}/feedback_variants.json", encoding="utf-8"
-        ) as json_file:
-            feedback_variants = json.load(json_file)[self.feedback_type]
-
-        self.feedback_data[self.feedback_type][self.feedback_mode] = {
-            f"{self.feedback_freq_type}_{self.feedback_freq_steps}": []
-        }
-        for e, episode in enumerate(self.episode_data["goal_positions"]):
-            episode_feedback = []
-            if self.feedback_freq_type.lower() == "poisson":
-                feedback_freq = 0
-                while feedback_freq == 0:
-                    feedback_freq = np.random.poisson(self.feedback_freq_steps)
-            else:
-                feedback_freq = self.feedback_freq_steps
-            for i, goal_position in enumerate(episode):
-                agent_position = self.episode_data["agent_positions"][e][i]
-                if i == 0:
-                    d_previous = self._get_distance(goal_position, agent_position)
-                if i % feedback_freq == 0 and i != 0:
-                    d_current = self._get_distance(goal_position, agent_position)
-                    polarity = self._get_polarity(d_current, d_previous)
-                    feedback = feedback_variants[polarity][self.feedback_mode]
-                    if not isinstance(feedback, str):
-                        random_id = self.rng.integers(len(feedback))
-                        random_feedback = feedback[random_id]
-                        episode_feedback.append(random_feedback)
-                    else:
-                        episode_feedback.append(feedback)
-                    d_previous = d_current
-                else:
-                    episode_feedback.append("")
-            self.feedback_data[self.feedback_type][self.feedback_mode][
-                f"{self.feedback_freq_type}_{self.feedback_freq_steps}"
-            ].append(episode_feedback)
+        return super().generate_feedback("agent_positions")
 
 
 class ActionFeedback(Feedback):
@@ -276,49 +255,14 @@ class ActionFeedback(Feedback):
             0, 3, dtype=int
         )  # not sure if done is actually needed?
 
-    def _get_polarity(self, action):
+    def _get_polarity(self, action, current_episode, current_step):
         if action in self.valid_actions:
             return "positive"
         else:
             return "negative"
 
     def generate_feedback(self):
-        with open(
-            f"{os.path.abspath('')}/feedback_variants.json", encoding="utf-8"
-        ) as json_file:
-            feedback_variants = json.load(json_file)[self.feedback_type]
-
-        self.feedback_data[self.feedback_type][self.feedback_mode] = {
-            f"{self.feedback_freq_type}_{self.feedback_freq_steps}": []
-        }
-        for episode in self.episode_data["actions"]:
-            episode_feedback = []
-            if self.feedback_freq_type.lower() == "poisson":
-                feedback_freq = 0
-                while feedback_freq == 0:
-                    feedback_freq = np.random.poisson(self.feedback_freq_steps)
-            else:
-                feedback_freq = self.feedback_freq_steps
-            for i, action in enumerate(episode):
-                if i % feedback_freq == 0:
-                    polarity = self._get_polarity(action)
-                    feedback = feedback_variants[polarity][self.feedback_mode]
-                    if not isinstance(feedback, str):
-                        random_id = self.rng.integers(len(feedback))
-                        random_feedback = feedback[random_id]
-                        episode_feedback.append(random_feedback)
-                    else:
-                        episode_feedback.append(feedback)
-                else:
-                    episode_feedback.append("")
-            self.feedback_data[self.feedback_type][self.feedback_mode][
-                f"{self.feedback_freq_type}_{self.feedback_freq_steps}"
-            ].append(episode_feedback)
-
-
-# class AdjacencyFeedback(Feedback):
-# def __init__(self, *args):
-#     super().__init__(*args)
+        return super().generate_feedback("actions")
 
 
 if __name__ == "__main__":
