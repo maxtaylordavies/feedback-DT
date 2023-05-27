fa9369b3b7eca9888a154fa57629f939c24c212b
Max Taylor-Davies <maxtaylordavies@gmail.com>
2023-05-27 12:34:17 +0100

    implements new atari evaluation code based on original paper

--- src/trainer/evaluator.py
+++ src/trainer/evaluator.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import cv2
 import gymnasium as gym
@@ -14,10 +15,85 @@ import seaborn as sns
 
 from src.agent import Agent, AgentInput, RandomAgent
 from src.utils.utils import discounted_cumsum, log
-
-# from .atari_env import AtariEnv
+from .atari_env import AtariEnv
 
 sns.set_theme()
+
+
+class Visualiser:
+    def __init__(
+        self,
+        env,
+        directory,
+        filename,
+        seed,
+        auto_release=True,
+        size=None,
+        fps=30,
+        rgb=True,
+    ):
+        self.env = env
+        self.directory = directory
+        self.path = os.path.join(self.directory, f"{filename}.mp4")
+        self.auto_release = auto_release
+        self.active = True
+        self.fps = fps
+        self.rgb = rgb
+
+        if size is None:
+            self.env.reset(seed=seed)
+            self.size = self.env.render().shape[:2][::-1]
+        else:
+            self.size = size
+
+    def pause(self):
+        self.active = False
+
+    def resume(self):
+        self.active = True
+
+    def _start(self):
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        self._writer = cv2.VideoWriter(self.path, fourcc, self.fps, self.size)
+
+    def _write(self):
+        if self.active:
+            frame = self.env.render()
+            if self.rgb:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self._writer.write(frame)
+
+    def release(self):
+        self._writer.release()
+
+    def reset(self, *args, **kwargs):
+        obs = self.env.reset(*args, **kwargs)
+        self._start()
+        self._write(obs)
+        return obs
+
+    def step(self, *args, **kwargs):
+        data = self.env.step(*args, **kwargs)
+
+        self._write()
+
+        if self.auto_release and data[2]:
+            self.release()
+
+        return data
+
+    def save_as_best(self):
+        shutil.copy(self.path, os.path.join(self.directory, "best.mp4"))
+
+
+class AtariVisualiser(Visualiser):
+    def _write(self, obs):
+        if not self.active:
+            return
+        frame = obs.numpy().reshape((self.env.window,) + self.size)[-1]
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+        frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        self._writer.write(frame)
 
 
 class Evaluator(TrainerCallback):
@@ -26,9 +102,9 @@ class Evaluator(TrainerCallback):
         user_args,
         collator,
         sample_interval=100,
-        target_returns=[0, 3, 90, 1000],
+        target_returns=[0, 90, 1000],
         num_repeats=10,
-        gamma=1.0,
+        gamma=0.99,
     ) -> None:
         super().__init__()
         self.user_args = user_args
@@ -36,9 +112,11 @@ class Evaluator(TrainerCallback):
         self.sample_interval = sample_interval
         self.samples_processed = 0
         self.target_returns = target_returns
+        self.best_return = -np.inf
         self.num_repeats = num_repeats
         self.gamma = gamma
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cpu")
 
         # create the output directory if it doesn't exist
         self.output_dir = os.path.join(self.user_args["output"], self.user_args["run_name"])
@@ -46,7 +124,7 @@ class Evaluator(TrainerCallback):
             os.makedirs(self.output_dir)
 
         # initialise a dataframe to store the results
-        self.results = {"samples": [], "return": [], "target_return": []}
+        self.results = {"samples": [], "return": [], "episode length": [], "model": []}
 
         # create blank feedback embeddings
         self.feedback_embeddings = self.collator._embed_feedback(
@@ -82,7 +160,7 @@ class Evaluator(TrainerCallback):
     def _run_eval_and_plot(self, agent: Agent, state: TrainerState, final=False):
         log(
             f"Running {'FINAL' if final else ''} evaluation (samples: {self.samples_processed}, epoch: {state.epoch}, step: {state.global_step})",
-            with_tqdm=True,
+            with_tqdm=False,
         )
 
         if final:
@@ -94,38 +172,52 @@ class Evaluator(TrainerCallback):
         if not final:
             self.samples_processed = self.collator.samples_processed
 
-    def _evaluate_agent(self, agent: Agent):
-        def create_env():
-            _env = gym.make(self.user_args["env_name"], render_mode="rgb_array")
-            return (
-                Visualiser(
-                    _env, self.output_dir, filename=f"{rep}", seed=self.user_args["seed"]
-                )
-                if self.user_args["record_video"]
-                else _env
+    def _create_env(self, atari=False):
+        env_name = self.user_args["env_name"]
+        if atari:
+            game = env_name.split("-")[1]
+            _env = AtariEnv(self.device, game, self.user_args["seed"])
+            return AtariVisualiser(
+                _env,
+                self.output_dir,
+                filename=f"tmp",
+                seed=self.user_args["seed"],
             )
 
-        def record_return(ret, target):
-            self.results["samples"].append(self.samples_processed)
-            self.results["return"].append(ret)
-            self.results["target_return"].append(target)
+        _env = gym.make(self.user_args["env_name"], render_mode="rgb_array")
+        return Visualiser(_env, self.output_dir, filename=f"tmp", seed=self.user_args["seed"])
+
+    def _record_return(self, env, ret, ep_length, model_name):
+        self.results["samples"].append(self.samples_processed)
+        self.results["return"].append(ret)
+        self.results["episode length"].append(ep_length)
+        self.results["model"].append(model_name)
+
+        if self.user_args["record_video"]:
+            env.release()
+
+        if ret > self.best_return:
+            self.best_return = ret
+            log(f"New best return: {ret}", with_tqdm=False)
+            if self.user_args["record_video"]:
+                env.save_as_best()
+
+    def _evaluate_agent(self, agent: Agent):
+        atari = self.user_args["env_name"].startswith("atari")
+        run_agent = self._run_agent_on_atari_env if atari else self._run_agent_on_env
 
         # for each repeat, record the episode return (and optionally render a video of the episode)
-        for rep in range(self.num_repeats):
+        for _ in range(self.num_repeats):
             # random baseline
-            env = create_env()
-            random_ret = self._run_agent_on_atari_env(self.random_agent, env, 0)
-            record_return(random_ret, "random")
-            if self.user_args["record_video"]:
-                env.release()
+            env = self._create_env(atari=atari)
+            ret, ep_length = run_agent(self.random_agent, env, 0)
+            self.record_return(env, ret, ep_length, "random")
 
+            # agent under evaluation
             for tr in self.target_returns:
-                env = create_env()
-                ret = self._run_agent_on_atari_env(agent, env, tr)
-                record_return(ret, tr)
-
-                if self.user_args["record_video"]:
-                    env.release()
+                env = self._create_env(atari=atari)
+                ret, ep_length = run_agent(agent, env, tr)
+                self.record_return(env, ret, ep_length, f"DT ({tr})")
 
         # convert results to dataframe
         df = pd.DataFrame(self.results)
@@ -133,13 +225,13 @@ class Evaluator(TrainerCallback):
         # log the average episode return for the current eval
         log(
             f"Average episode return: {df[df['samples'] == self.samples_processed]['return'].mean()}",
-            with_tqdm=True,
+            with_tqdm=False,
         )
 
         # save the results to disk
         df.to_pickle(os.path.join(self.output_dir, "returns.pkl"))
 
-    def _run_agent_on_env(self, agent: Agent, env: gym.Env):
+    def _run_agent_on_env(self, agent: Agent, env: Visualiser):
         max_ep_len = env.max_steps if hasattr(env, "max_steps") else 1000
 
         state_mean = torch.from_numpy(self.collator.state_mean.astype(np.float32)).to(
@@ -215,23 +307,14 @@ class Evaluator(TrainerCallback):
         return discounted_cumsum(rewards.detach().cpu().numpy(), self.gamma)[0]
 
     def _run_agent_on_atari_env(
-        self, agent: Agent, env: gym.Env, target_return: int, stack_size=4
+        self, agent: Agent, env: AtariVisualiser, target_return: int, stack_size=4
     ):
-        def get_frame(rgb_obs):
-            return cv2.resize(
-                np.dot(rgb_obs[..., :3], [0.299, 0.587, 0.114]),
-                (84, 84),
-                interpolation=cv2.INTER_LINEAR,
-            )
-
         def get_state(frames):
-            return (
-                torch.from_numpy(np.stack(frames, axis=0))
-                .reshape(1, self.collator.state_dim)
-                .to(device=self.device, dtype=torch.float32)
+            return frames.reshape(1, self.collator.state_dim).to(
+                device=self.device, dtype=torch.float32
             )
 
-        max_ep_len = env.max_steps if hasattr(env, "max_steps") else 100
+        max_ep_len = env.max_steps if hasattr(env, "max_steps") else 1000
 
         state_mean = torch.from_numpy(self.collator.state_mean.astype(np.float32)).to(
             device=self.device
@@ -240,13 +323,10 @@ class Evaluator(TrainerCallback):
             device=self.device
         )
 
-        obs, _ = env.reset(seed=self.user_args["seed"])
-        # fully_obs_env = FullyObsWrapper(env)
-        # obs = fully_obs_env.observation(tmp[0])
+        (obs,) = env.reset(seed=self.user_args["seed"])
+        states = get_state(obs)
 
-        states = get_state([get_frame(obs)] * stack_size)
-
-        # print(f"states.shape: {states.shape}")
+        print(f"states.shape: {states.shape}")
 
         actions = torch.zeros(
             (0, self.collator.act_dim), device=self.device, dtype=torch.float32
@@ -278,17 +358,13 @@ class Evaluator(TrainerCallback):
             )
             a = actions[-1].detach().cpu().numpy()
 
-            frames, reward = [], 0
-            for _ in range(stack_size):
-                obs, r, done, _, _ = env.step(np.argmax(a))
-                frames.append(get_frame(obs))
-                reward += r
+            obs, r, done, _, _ = env.step(np.argmax(a))
 
-            cur_state = get_state(frames)
+            cur_state = get_state(obs)
             states = torch.cat([states, cur_state], dim=0)
 
-            rewards[-1] = reward
-            pred_return = returns_to_go[0, -1] - reward
+            rewards[-1] = r
+            pred_return = returns_to_go[0, -1] - r
             returns_to_go = torch.cat([returns_to_go, pred_return.reshape(1, 1)], dim=1)
 
             timesteps = torch.cat(
@@ -302,7 +378,7 @@ class Evaluator(TrainerCallback):
             if done:
                 break
 
-        return discounted_cumsum(rewards.detach().cpu().numpy(), self.gamma)[0]
+        return discounted_cumsum(rewards.detach().cpu().numpy(), self.gamma)[0], t
 
     def _plot_loss(self, state: TrainerState):
         fig, ax = plt.subplots()
@@ -317,66 +393,3 @@ class Evaluator(TrainerCallback):
         sns.lineplot(x="samples", y="return", hue="target_return", data=df, ax=ax)
         fig.savefig(os.path.join(self.output_dir, "returns.png"))
         plt.close(fig)
-
-
-class Visualiser(gym.Wrapper):
-    def __init__(
-        self,
-        env,
-        directory,
-        filename,
-        seed,
-        auto_release=True,
-        size=None,
-        fps=30,
-        rgb=True,
-    ):
-        super(Visualiser, self).__init__(env)
-        self.directory = directory
-        self.path = os.path.join(self.directory, f"{filename}.mp4")
-        self.auto_release = auto_release
-        self.active = True
-        self.fps = fps
-        self.rgb = rgb
-
-        if size is None:
-            self.env.reset(seed=seed)
-            self.size = self.env.render().shape[:2][::-1]
-        else:
-            self.size = size
-
-    def pause(self):
-        self.active = False
-
-    def resume(self):
-        self.active = True
-
-    def _start(self):
-        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        self._writer = cv2.VideoWriter(self.path, fourcc, self.fps, self.size)
-
-    def _write(self):
-        if self.active:
-            frame = self.env.render()
-            if self.rgb:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            self._writer.write(frame)
-
-    def release(self):
-        self._writer.release()
-
-    def reset(self, *args, **kwargs):
-        observation = self.env.reset(*args, **kwargs)
-        self._start()
-        self._write()
-        return observation
-
-    def step(self, *args, **kwargs):
-        data = self.env.step(*args, **kwargs)
-
-        self._write()
-
-        if self.auto_release and data[2]:
-            self.release()
-
-        return data
