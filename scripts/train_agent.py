9e3214d9b3977d6e1b5e5c1718f4343f9a3ba932
Max Taylor-Davies <maxtaylordavies@gmail.com>
2023-05-29 19:50:56 +0100

    updates atari training script

--- scripts/train_agent.py
+++ scripts/train_agent.py
@@ -6,22 +6,30 @@ from src.dataset.custom_dataset import CustomDataset
 from src.collator import Collator
 from src.agent.fdt import AtariFDTAgent
 from src.trainer import AgentTrainer
-from src.utils.utils import setup_devices
+from src.utils.utils import setup_devices, log
+
+log("imports done")
 
 os.environ["WANDB_DISABLED"] = "true"
 
 DATA_DIR = "/home/s2227283/projects/feedback-DT/data/dqn_replay"
 GAME = "Breakout"
-NUM_SAMPLES = 1000
+NUM_SAMPLES = 10000
 CONTEXT_LENGTH = 30
 BATCH_SIZE = 32
 EPOCHS = 10
-SEED = 42
+SEED = 123
 
+log("setting up devices")
 device = setup_devices(SEED, useGpu=True)
 
+log("creating dataset")
 dataset = CustomDataset.from_dqn_replay(DATA_DIR, GAME, NUM_SAMPLES)
+
+log("creating collator")
 collator = Collator(custom_dataset=dataset, feedback=None, context_length=CONTEXT_LENGTH)
+
+log("creating agent")
 agent = AtariFDTAgent(
     config=DecisionTransformerConfig(
         state_dim=collator.state_dim,
@@ -30,15 +38,17 @@ agent = AtariFDTAgent(
         max_length=CONTEXT_LENGTH,
     )
 )
+
+log("creating trainer")
 trainer = AgentTrainer(
     agent=agent,
     collator=collator,
     dataset=dataset,
     args={
         "run_name": "breakout-test-1",
-        "env_name": "ALE/Breakout-v5",
+        "env_name": "atari:Breakout",
         "seed": SEED,
-        "output": ".",
+        "output": "/home/s2227283/projects/feedback-DT/data/output",
         "wandb_mode": "disabled",
         "report_to": "none",
         "log_interval": 10,
@@ -47,7 +57,9 @@ trainer = AgentTrainer(
         "lr": 1e-4,
         "context_length": CONTEXT_LENGTH,
         "plot_on_train_end": True,
-        "record_video": False,
+        "record_video": True,
     },
 )
+
+log("training")
 trainer.train()
