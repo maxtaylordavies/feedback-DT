b56d744b692d0bc6d1fc2d213d0ebd73cc1e58cc
Sabrina McCallum <sabrina.mccallum@outlook.com>
2023-08-31 15:21:08 +0100

    fix job files COMMAND string

--- scripts/experiments/test/job_agents_servers.sh
+++ scripts/experiments/test/job_agents_servers.sh
@@ -94,7 +94,7 @@ source ${VENV_NAME}/bin/activate
 # inclusive.
 
 experiment_text_file=$1
-COMMAND="'sed \"${SLURM_ARRAY_TASK_ID}q;d\" ${experiment_text_file}'"
+COMMAND="`sed \"${SLURM_ARRAY_TASK_ID}q;d\" ${experiment_text_file}`"
 echo "Running provided command: ${COMMAND}"
 eval "${COMMAND}"
 echo "Command ran successfully!"
