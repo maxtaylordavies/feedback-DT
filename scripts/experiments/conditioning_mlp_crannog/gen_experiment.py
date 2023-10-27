380fe28f904732c4cb4825e6a9fcd93534609b6f
Sabrina McCallum <sabrina.mccallum@outlook.com>
2023-10-27 10:35:01 +0100

    Pre experiment tests - final changes from across servers (#73)
    * update dataset len logic and arg defaults
    
    * always sample last step of episode
    
    * reduce eps/shard for hard lev for longer cont len
    
    * require explicit disabling of rtg
    
    * create rand datasets from n seeds / eps per seeds
    
    * reate rand datasets from n seeds / eps per seeds
    
    * enable diff mission/feedback embeds at inference
    
    * fix sentence embedding for inference
    
    * fix sentence embeddings for inference (2)
    
    * allow for random mission / using mission at inf
    
    * update train script
    
    * fix shape mismatch for mission embeds at inference
    
    * pass custom loss mean option as arg
    
    * new experiments and arg printing
    
    * use fewer eps per shard
    
    * align format of args values with analysis script
    
    * use actual mission/num feedb emb at inf by default
    
    * turn analysis script into jupyter nb
    
    * create exp for symb obs / change some arg defaults
    
    * create experiment folders for all servers
    
    * update local test file for rgb obs exp
    
    * fix issue with passing False in command line args
    
    * use value of rgb_obs arg for state embedding model
    
    * pass use_rgb to MinigridFDTAgent in train script
    
    * fix chevrons in new mlp experiments from merge
    
    * update local train script for symbolic obs use
    
    * update dataset naming to distinguish rgb/symb obs
    
    * fix missing string in dataset naming
    
    * fix dataset naming
    
    * fix dataset naming
    
    * set frame size based on type of obs (symb vs rgb)
    
    * rm curriculum refs, fix self.full / self.ep_dist
    
    * loop thru shards when eps_per_shard < batch_size
    
    * ensure we are sampling from every shard each epoch
    
    * create python script version of analysis script
    
    * reset shard before eval to free up memory
    
    * eval on train begin (uncomment after testing)
    
    * select seeds for ood excl. (no multi-type seeds)
    
    * sample evenly by ood type (not config)
    
    * remove tanh layer for discrete action outputs
    
    * disable tanh for actions in DT config
    
    * set eval intervals based on steps/eval on step end
    
    * pass data and model seeds as TrainingArguments
    
    * some small fixes to default args and logging
    
    * create experiment files for second seed
    
    * remove surplus job files
    
    * update MLP experiment files
    
    * change patience to 20 / upd dir in job.sh
    
    * add exp files for dmog
    
    ---------
    
    Co-authored-by: “Sabrina <“sabrina.mccallum@outlook.com”>
    Co-authored-by: Sabrina McCallum <sm2152@login1.pri.dmog.alces.network>

--- scripts/experiments/conditioning_mlp_crannog/gen_experiment.py
+++ scripts/experiments/conditioning_mlp_crannog/gen_experiment.py
@@ -27,8 +27,7 @@ def run_name(combo, keys):
 
 
 # this is the base command that will be used for the experiment
-base_call = f"python {PROJECT_HOME}/scripts/train_agent_babyai.py -o {DATA_HOME}/output --load_existing_dataset True"
-# --eps_per_shard 4
+base_call = f"python {PROJECT_HOME}/scripts/train_agent_babyai.py -o {DATA_HOME}/output --load_existing_dataset True --early_stopping_patience 20"
 
 # define a dictionary of variables to perform a grid search over.
 # the key for each variable should match the name of the command-line
@@ -63,7 +62,7 @@ variables = {
         False
     ],
      "model_seed": [
-        987654321, 
+        987654321,
     ]
 }
 
@@ -81,6 +80,7 @@ for c in combinations:
         expt_call += f" --{var} {c[i]}"
 
     expt_call += f" --run_name {run_name(c, variables.keys())}"
+    expt_call = f"export RUN_NAME={run_name(c, variables.keys())}; " + expt_call
     print(expt_call, file=output_file)
 
 output_file.close()
