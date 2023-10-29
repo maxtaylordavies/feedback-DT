a88e72349e65ca349d5e814bd1fa7a167da1f13d
Sabrina McCallum <sabrina.mccallum@outlook.com>
2023-10-29 21:48:45 +0000

    allow for exps on servers w & w/o scratch disk

--- scripts/train_agent_babyai.py
+++ scripts/train_agent_babyai.py
@@ -13,16 +13,12 @@ from src.utils.argparsing import get_args
 from src.utils.utils import frame_size
 from src.utils.utils import log
 from src.utils.utils import seed
-
-# from src.constants import OUTPUT_PATH
-
 os.environ["WANDB_DISABLED"] = "true"
 os.environ["ENV_METADATA_PATH"] = ENV_METADATA_PATH
 
 seed(GLOBAL_SEED)
 
 args = get_args()
-args["level"] = "PickupLoc"
 
 for arg, value in args.items():
     print(f"{arg:}\n {value} \n{'==='*20}")
@@ -39,7 +35,7 @@ if torch.cuda.is_available():
     device_str = f"{device.type}:{device.index}" if device.index else f"{device.type}"
     os.environ["CUDA_VISIBLE_DEVICES"] = device_str
     # log(f"Using device: {torch.cuda.get_device_name()}")
-    log("using gpu")
+    log("Using gpu")
 elif not torch.backends.mps.is_available():
     if not torch.backends.mps.is_built():
         print(
@@ -52,43 +48,43 @@ elif not torch.backends.mps.is_available():
             "and/or you do not have an MPS-enabled device on this machine."
         )
     device = torch.device("cpu")
-    log("using cpu")
+    log("Using cpu")
 else:
     device = torch.device("mps")
-    log("using mps")
+    log("Using mps")
 
 log("getting dataset...")
 dataset = CustomDataset.get_dataset(args)
-# log("creating collator...")
-# collator = Collator(
-#     custom_dataset=dataset,
-#     args=args,
-# )
+log("creating collator...")
+collator = Collator(
+    custom_dataset=dataset,
+    args=args,
+)
 
-# seed(args["model_seed"])
+seed(args["model_seed"])
 
-# log("creating agent...")
-# agent = MinigridFDTAgent(
-#     config=DecisionTransformerConfig(
-#         state_dim=collator.state_dim,
-#         act_dim=collator.act_dim,
-#         state_shape=(3, frame_size, frame_size),
-#         max_length=args["context_length"],
-#     ),
-#     use_missions=args["use_mission"],
-#     use_feedback=args["use_feedback"],
-#     use_rtg=args["use_rtg"],
-#     loss_mean_type=args["loss_mean_type"],
-#     use_rgb=args["rgb_obs"]
-# )
+log("creating agent...")
+agent = MinigridFDTAgent(
+    config=DecisionTransformerConfig(
+        state_dim=collator.state_dim,
+        act_dim=collator.act_dim,
+        state_shape=(3, frame_size, frame_size),
+        max_length=args["context_length"],
+    ),
+    use_missions=args["use_mission"],
+    use_feedback=args["use_feedback"],
+    use_rtg=args["use_rtg"],
+    loss_mean_type=args["loss_mean_type"],
+    use_rgb=args["rgb_obs"]
+)
 
-# log("creating trainer...")
-# trainer = AgentTrainer(
-#     agent=agent,
-#     collator=collator,
-#     dataset=collator.dataset,
-#     args=args,
-# )
+log("creating trainer...")
+trainer = AgentTrainer(
+    agent=agent,
+    collator=collator,
+    dataset=collator.dataset,
+    args=args,
+)
 
-# log("training agent...")
-# trainer.train()
+log("training agent...")
+trainer.train()
