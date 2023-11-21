ae7e5486857eaa290bb76810336c473008820f43
Sabrina McCallum <sabrina.mccallum@outlook.com>
2023-11-21 16:38:57 +0000

    README and analysis scripts (#83)
    * remove references to grad prop for img encoder
    
    * enable zeroing out of feedback at inference
    
    * include only gcrl submission levels in experiments
    
    * tweak feedback text
    
    * add GoToObj level to experiments
    
    * implement mean embedding at inference
    
    * dynamically adjust early stopping patience
    
    * make script to extract results.pkl from exp output
    
    * update analysis script/create ipynb version
    
    * remove legacy args
    
    * save inference mode diffs
    
    * round values in results tables
    
    * adds docstrings to src/agent
    
    * adds docstrings to src/collator
    
    * adds docstrings to src/dataset
    
    * adds docstrings to src/env
    
    * adds docstrings to src/ppo
    
    * adds docstrings to src/trainer
    
    * adds docstrings to src/utils
    
    * updates readme
    
    * small tweaks to README
    
    * adds brief section on FeedbackEnv
    
    * refact analysis script / aggr by maze/single room
    
    * include manual controller section in readme
    
    ---------
    
    Co-authored-by: Max Taylor-Davies <maxtaylordavies@gmail.com>

--- scripts/analyse_results.py
+++ scripts/analyse_results.py
@@ -1,19 +1,15 @@
 import os
 import warnings
 
+import numpy as np
 import pandas as pd
 import seaborn as sns
-from IPython.display import display
 from matplotlib import pyplot as plt
-
-from src.utils.argparsing import get_args
-
-pd.set_option('display.max_rows', 500)
 
 warnings.filterwarnings("ignore")
 sns.set(font="Helvetica")
-USER = os.environ["USER"]
-ROOT = os.path.dirname(os.path.abspath(USER))
+ROOT = os.getcwd().split("scripts")[0]
+
 
 def get_conditioning(row):
     """
@@ -30,6 +26,7 @@ def get_conditioning(row):
         else:
             conditioning.append("all feedback")
     return " + ".join(conditioning)
+
 
 def get_experiments(output_path, dir):
     """
@@ -63,6 +60,7 @@ def get_experiments(output_path, dir):
         dfs.append(df)
     return dfs
 
+
 def get_combined_df(output_path, level=None):
     """
     Returns a dataframe containing the results of all experiments in a given directory.
@@ -76,6 +74,7 @@ def get_combined_df(output_path, level=None):
             dfs.extend(current_dfs)
     dfs = [df[(df["eval_type"] != "efficiency") & (df["model"] == "DT")] for df in dfs]
     return pd.concat(dfs, ignore_index=True)
+
 
 def aggr_results(df, inference_mode, eval_type, metric):
     """
@@ -91,7 +90,7 @@ def aggr_results(df, inference_mode, eval_type, metric):
     ]
 
     results = (
-        filtered_results[["conditioning",  metric]]
+        filtered_results[["conditioning", metric]]
         .groupby(["conditioning"])
         .agg(
             Mean=(metric, "mean"),
@@ -103,13 +102,13 @@ def aggr_results(df, inference_mode, eval_type, metric):
     results["Mean"] = results["Mean"].round(4)
     results["Std"] = results["Std"].round(4)
 
-    results["ood_type"] = 'all types' if eval_type == "ood_generalisation" else None
+    results["ood_type"] = "all types" if eval_type == "ood_generalisation" else None
     results["eval_type"] = eval_type
     results["feedback_at_inference"] = inference_mode
 
     if eval_type == "ood_generalisation":
         results_by_ood = (
-            filtered_results[["ood_type", "conditioning",  metric]]
+            filtered_results[["ood_type", "conditioning", metric]]
             .groupby(["ood_type", "conditioning"])
             .agg(
                 Mean=(metric, "mean"),
@@ -127,6 +126,7 @@ def aggr_results(df, inference_mode, eval_type, metric):
         results_by_ood = None
 
     return results, results_by_ood
+
 
 def get_deltas(df, reference):
     """
@@ -156,7 +156,9 @@ def combine_results(df, metric):
             if pd.isna(inference_mode):
                 continue
             for eval_type in df["eval_type"].unique():
-                results, results_by_ood = aggr_results(df, inference_mode, eval_type, metric)
+                results, results_by_ood = aggr_results(
+                    df, inference_mode, eval_type, metric
+                )
                 results.reset_index(inplace=True)
 
                 results_with_deltas = get_deltas(results, reference)
@@ -165,9 +167,13 @@ def combine_results(df, metric):
                 if results_by_ood is not None:
                     results_by_ood.reset_index(inplace=True)
                     for ood_type in results_by_ood["ood_type"].unique():
-                        results_by_ood_with_deltas = get_deltas(results_by_ood[results_by_ood["ood_type"] == ood_type], reference)
+                        results_by_ood_with_deltas = get_deltas(
+                            results_by_ood[results_by_ood["ood_type"] == ood_type],
+                            reference,
+                        )
                         dfs.append(results_by_ood_with_deltas)
     return pd.concat(dfs, ignore_index=True)
+
 
 def save_results_as_csv(df, level, metric, output_path):
     """
@@ -175,6 +181,7 @@ def save_results_as_csv(df, level, metric, output_path):
     """
     path = os.path.join(output_path, f"results_{level}_{metric}.csv")
     df.to_csv(path)
+
 
 def map_colors(df, colors):
     """
@@ -201,9 +208,9 @@ def set_axis_ticks(ax, reference, reference_perf, ylims, sizing_config):
     yticklabels = ax.get_yticks().tolist()
 
     new_yticklabels = [
-        f"{reference_perf:.2f}*"
+        f"{reference_perf*100:.2f}*"
         if float(ytick) == float(0)
-        else (f"+{ytick:.2f}" if float(ytick) > float(0) else f"{ytick:.2f}")
+        else (f"+{ytick*100:.2f}" if float(ytick) > float(0) else f"{ytick*100:.2f}")
         for ytick in yticklabels
     ]
     ax.set_yticklabels(new_yticklabels, fontsize=sizing_config["tick_label_size"])
@@ -217,6 +224,7 @@ def set_axis_ticks(ax, reference, reference_perf, ylims, sizing_config):
     ax.set_xticklabels(new_xticklabels, fontsize=sizing_config["tick_label_size"])
     plt.tight_layout(pad=0.5)
 
+
 def set_bar_values(ax, df, ylims, sizing_config):
     """
     Sets the values of the bars on top of the bars (for positive deltas)
@@ -229,7 +237,7 @@ def set_bar_values(ax, df, ylims, sizing_config):
         ax.text(
             x_pos,
             y_pos,
-            f"{value:.2f}",
+            f"{value*100:.2f}",
             ha="center",
             va=va,
             fontsize=sizing_config["value_size"],
@@ -241,8 +249,8 @@ def set_pad_title(level, eval_type, inference_mode, sizing_config):
     """
     Sets the title of the plot, with padding.
     """
-    if "all" in level:
-        level = level.replace('_', ' ')
+    if "all" in level.lower() or "single" in level.lower() or "maze" in level.lower():
+        level = level
     else:
         level = "level " + f"'{level}'"
     plt.title(
@@ -415,6 +423,7 @@ def plot_deltas_with_text(
     plt.margins(x=0.025, y=0.025)
     plt.tight_layout(pad=0.5)
 
+
 def plot_deltas(
     df,
     level,
@@ -448,22 +457,31 @@ def plot_deltas(
         color_palette,
     )
     plt.savefig(
-        os.path.join(output_path, f"{level}_{metric}_{eval_type}_{reference}_{inference_mode}.png"),
-        bbox_inches="tight",
-    )
-    plot_deltas_without_text(sizing_config, df, ylims, color_palette)
-    plt.savefig(
         os.path.join(
-            output_path, f"{level}_{metric}_{eval_type}_{reference}_{inference_mode}_no_text.png"
+            output_path,
+            f"{level}_{metric}_{eval_type}_{reference}_{inference_mode}.png",
         ),
         bbox_inches="tight",
     )
+    plt.close()
+    plot_deltas_without_text(sizing_config, df, ylims, color_palette)
+    plt.savefig(
+        os.path.join(
+            output_path,
+            f"{level}_{metric}_{eval_type}_{reference}_{inference_mode}_no_text.png",
+        ),
+        bbox_inches="tight",
+    )
+    plt.close()
+
 
 def plot_results(df, level, metric, colors, size, output_path):
     """
     Plots the results of a given dataframe.
     """
-    ylims = max(abs(df["Delta (Mean)"].max()), abs(df["Delta (Mean)"].min())) * 1.25
+    ylims = max(abs(df["Delta (Mean)"].max()), abs(df["Delta (Mean)"].min())) * (
+        1.25 if not "all" in level else 1.05
+    )
     for reference in ["mission", "rtg"]:
         for eval_type in df["eval_type"].unique():
             for inference_mode in df["feedback_at_inference"].unique():
@@ -476,20 +494,28 @@ def plot_results(df, level, metric, colors, size, output_path):
                             (df["eval_type"] == eval_type)
                             & (df["reference"] == reference)
                             & (pd.isna(df["ood_type"]))
-                            & ((df["feedback_at_inference"] == inference_mode) | (pd.isna(df["feedback_at_inference"])))
+                            & (
+                                (df["feedback_at_inference"] == inference_mode)
+                                | (pd.isna(df["feedback_at_inference"]))
+                            )
                         ]
                     else:
                         results_with_ref = df[
                             (df["eval_type"] == eval_type)
                             & (df["reference"] == reference)
                             & (df["ood_type"] == ood_type)
-                            & ((df["feedback_at_inference"] == inference_mode) | (pd.isna(df["feedback_at_inference"])))
+                            & (
+                                (df["feedback_at_inference"] == inference_mode)
+                                | (pd.isna(df["feedback_at_inference"]))
+                            )
                         ]
 
                     reference_perf = results_with_ref[
                         results_with_ref["conditioning"] == reference
                     ]["Mean"].values[0]
-                    results = results_with_ref[~df["conditioning"].isin(["mission", "rtg"])]
+                    results = results_with_ref[
+                        ~df["conditioning"].isin(["mission", "rtg"])
+                    ]
                     results.sort_values("conditioning", inplace=True)
 
                     plot_deltas(
@@ -508,7 +534,7 @@ def plot_results(df, level, metric, colors, size, output_path):
                     )
 
 
-def get_inference_mode_diffs(results_df):
+def get_inference_mode_diffs(results_df, level):
     x = results_df.sort_values(
         by=[
             "reference",
@@ -519,84 +545,175 @@ def get_inference_mode_diffs(results_df):
         ],
         ascending=False,
     )
-    x["diff"] = x["Mean"].diff()
+    x["diff"] = x["Delta (Mean)"].diff()
     x["diff"] = x["diff"].apply(lambda x: round(x, 4))
     x["diff"][x["feedback_at_inference"] == "numerical"] = 0
     inference_mode_diffs = x[
+        [
+            "conditioning",
+            "eval_type",
+            "ood_type",
+            "diff",
+        ]
+    ][
+        (~x["conditioning"].isin(["mission", "rtg"]))
+        & (x["feedback_at_inference"] != "numerical")
+    ]
+    inference_mode_diffs_new = inference_mode_diffs.merge(
+        results_df[
+            ["conditioning", "eval_type", "ood_type", "reference", "Delta (Mean)"]
+        ][results_df["feedback_at_inference"] == "numerical"],
+        on=["conditioning", "eval_type", "ood_type"],
+    ).drop_duplicates()
+    inference_mode_diffs_new["level"] = level
+    inference_mode_diffs_rtg = inference_mode_diffs_new[
+        inference_mode_diffs_new["reference"] == "rtg"
+    ]
+    inference_mode_diffs_mission = inference_mode_diffs_new[
+        inference_mode_diffs_new["reference"] == "mission"
+    ]
+    return inference_mode_diffs_rtg, inference_mode_diffs_mission
+
+
+def plot_diffs_vs_numerical_performance(df, level, output_path):
+    """
+    Plot correllation between differences in performance of conditioning variants with actual feedback at inference and numerical performance.
+    """
+    fig, ax = plt.subplots(figsize=(6, 6))
+    sns.scatterplot(
+        data=df,
+        x="Delta (Mean)",
+        y="diff",
+        hue="conditioning",
+        ax=ax,
+    )
+    for c in df["conditioning"].unique():
+        cdf = df[df["conditioning"] == c]
+        m, b = np.polyfit(cdf["Delta (Mean)"], cdf["diff"], 1)
+        plt.plot(cdf["Delta (Mean)"], m * cdf["Delta (Mean)"] + b)
+    plt.savefig(
+        os.path.join(output_path, f"{level}_diffs_vs_numerical_performance.png"),
+        bbox_inches="tight",
+    )
+    plt.close()
+
+
+def plot_and_save_results(df, level, metric, colors, size, output_path):
+    print(level.upper())
+    if "single" in level.lower():
+        filtered_df = df[
+            df["level"].isin(["GoToObj", "GoToLocal", "PutNextLocal", "PickupLoc"])
+        ]
+    elif "maze" in level.lower():
+        filtered_df = df[
+            df["level"].isin(["GoToObj", "GoToLocal", "PutNextLocal", "PickupLoc"])
+        ]
+    elif "all" in level.lower():
+        filtered_df = df
+    else:
+        filtered_df = df[df["level"] == level]
+    results = combine_results(filtered_df, metric)
+    inference_mode_diffs_rtg, inference_mode_diffs_mission = get_inference_mode_diffs(
+        results, level
+    )
+    save_results_as_csv(results, level, metric, output_path)
+    save_results_as_csv(
+        inference_mode_diffs_rtg, level, "inference_mode_diff_rtg", output_path
+    )
+    save_results_as_csv(
+        inference_mode_diffs_mission,
+        level,
+        "inference_mode_diff_mission",
+        output_path,
+    )
+    plot_results(results, level, metric, colors, size, output_path)
+    plot_diffs_vs_numerical_performance(inference_mode_diffs_rtg, level, output_path)
+    plot_diffs_vs_numerical_performance(inference_mode_diffs_mission, level, output_path)
+    print("Done!")
+    return inference_mode_diffs_rtg, inference_mode_diffs_mission
+
+
+def save_all_inference_mod_diffs(df, output_path):
+    all_inference_mode_diffs = (
+        pd.concat(df)
+        .sort_values(
+            by=["conditioning", "reference", "eval_type", "ood_type", "level"]
+        )[
             [
                 "conditioning",
+                "level",
+                "reference",
                 "eval_type",
                 "ood_type",
+                "Delta (Mean)",
                 "diff",
             ]
-        ][(~x["conditioning"].isin(["mission", "rtg"])) & (x["feedback_at_inference"] != "numerical")]
-    display(
-        inference_mode_diffs
+        ]
+        .reset_index(drop=True)
     )
-    return inference_mode_diffs
-
-if __name__ == "__main__":
-    args = get_args()
-    metric = args["analysis_metric"]
-    only_level = args["analyse_only_level"]
-
-    colors = {
-        "aarg_colors": [
-            "#2F8374",
-            "#499385",
-            "#63A297",
-            "#7DB2A8",
-            "#97C1BA",
-            "#B1D1CB",
-            "#CBE0DC",
-            "#E5F0EE",
-        ],
-        "hwu_colors": [
-            "#970E53",
-            "#A42C69",
-            "#B14A7E",
-            "#BE6894",
-            "#CB87A9",
-            "#D8A5BF",
-            "#E5C3D4",
-            "#F2E1EA",
-        ],
-        "uoe_si_colors": [
-            "#004f71",
-            "#206583",
-            "#407B94",
-            "#6091A6",
-            "#80A7B8",
-            "#9FBDCA",
-            "#BFD3DB",
-            "#DFE9ED",
-        ],
-    }
-    experiment_name = args["experiment_name"]
-    size = args["analysis_fig_size"]
-    data_home = f"{ROOT}/data/{experiment_name}/output"
-    output_path = f"{ROOT}/data/{experiment_name}/output/results"
-
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    all_inference_mode_diffs["diff"] = all_inference_mode_diffs["diff"].apply(
+        lambda x: "+" + f"{round(x*100, 2)}" if x > 0 else f"{round(x*100, 2)}"
+    )
+    all_inference_mode_diffs["Delta (Mean)"] = all_inference_mode_diffs[
+        "Delta (Mean)"
+    ].apply(lambda x: "+" + f"{round(x*100, 2)}" if x > 0 else f"{round(x*100, 2)}")
+    save_results_as_csv(
+        all_inference_mode_diffs, "all_levels", "all_inference_mode_diffs", output_path
+    )
 
 
-    comb_df = get_combined_df(data_home, level=only_level)
+metric ="gc_success"
+level = ""
 
-    for level in sorted(comb_df["level"].unique()):
-        print(level.upper())
-        level_df = comb_df[comb_df["level"] == level]
-        results_df = combine_results(level_df, metric)
-        inference_mode_diffs = get_inference_mode_diffs(results_df)
-        save_results_as_csv(inference_mode_diffs, level, "inference_mode_diff", output_path)
-        save_results_as_csv(results_df, level, metric, output_path)
-        plot_results(results_df, level, metric, colors, size, output_path)
-        print("===" * 10)
+colors = {
+    "aarg_colors": [
+        "#2F8374",
+        "#499385",
+        "#63A297",
+        "#7DB2A8",
+        "#97C1BA",
+        "#B1D1CB",
+        "#CBE0DC",
+        "#E5F0EE",
+    ],
+    "hwu_colors": [
+        "#970E53",
+        "#A42C69",
+        "#B14A7E",
+        "#BE6894",
+        "#CB87A9",
+        "#D8A5BF",
+        "#E5C3D4",
+        "#F2E1EA",
+    ],
+    "uoe_si_colors": [
+        "#004f71",
+        "#206583",
+        "#407B94",
+        "#6091A6",
+        "#80A7B8",
+        "#9FBDCA",
+        "#BFD3DB",
+        "#DFE9ED",
+    ],
+}
+experiment_name = "conditioning"
+size = "small-half"
+data_home = f"{ROOT}/data/{experiment_name}/output"
+output_path = f"{ROOT}/data/{experiment_name}/output/results"
 
-    if not only_level:
-        print("ALL LEVELS")
-        results_df = combine_results(comb_df, metric)
-        inference_mode_diffs = get_inference_mode_diffs(results_df)
-        save_results_as_csv(inference_mode_diffs, "all_levels", "inference_mode_diff", output_path)
-        save_results_as_csv(results_df, "all_levels", metric, output_path)
-        plot_results(results_df, "all_levels", metric, colors, size, output_path)
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
+
+comb_df = get_combined_df(data_home, level="")
+plot_categories = sorted(comb_df["level"].unique().tolist())
+plot_categories.extend(["AllLevels", "SingleRooms", "Mazes"])
+
+
+inference_mode_diffs_dfs = []
+for category in plot_categories:
+    inference_mode_diffs_rtg, inference_mode_diffs_mission = plot_and_save_results(comb_df, category, metric, colors, size, output_path)
+    inference_mode_diffs_dfs.append(inference_mode_diffs_rtg)
+
+save_all_inference_mod_diffs(inference_mode_diffs_dfs, output_path)
